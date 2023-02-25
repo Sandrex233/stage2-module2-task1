5953527cc47a50fd9f05f2b3f23f6686f39ff55f
Sandrex233 <sandrex233@gmail.com>
2023-02-25 19:13:14 +0400

    solution

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,22 +1,19 @@
 package com.example.servlet;
 
-import com.example.User;
-import com.example.Warehouse;
-
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/add")
 public class AddUserServlet extends HttpServlet {
     //write your code here!
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String firstName = req.getParameter("firstName");
-        String lastName = req.getParameter("lastName");
-        User user = new User(firstName, lastName);
-        Warehouse.getInstance().addUser(user);
-        resp.sendRedirect(req.getContextPath() + "/users");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("add.jsp");
+        dispatcher.forward(request, response);
     }
 }
